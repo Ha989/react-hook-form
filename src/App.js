7169f6e66ea34c5d5893d81ef2f46c0c1a2f7c36
Ha989 <viethanguyen0809@gmail.com>
2023-01-03 11:01:37 +0700

    react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useForm} from 'react-hook-form';
 import './App.css';
 
 function App() {
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
+  const onSubmit = data =>
+  console.log(data) 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> React Hook Form</h1>
+      <div className='container'>
+      <hr/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      
+      <div>
+        <label htmlFor='username'>Username</label>
+        <br/>
+        <input 
+        className='plholder'
+        name='username'
+        type="text"
+        autoComplete='off'
+        {...register('username')}
+        /> 
+      </div>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <br/>
+        <input 
+        className='plholder'
+        name='email'
+        type="text"
+        autoComplete='off'
+        {...register("email", {required: true})}
+        /> 
+        {errors.email && <p>Email is required!</p>}
+      </div>
+      <div>
+        <label>Language{" "}
+           <select {...register('language')}>
+           <option value='VN'>VN</option>
+           <option value='EN'>EN</option>
+           <option value='DE'>DE</option>
+           </select>
+        </label>
+       </div>
+       <input className="submit" type="submit" />
+       </form>
+       </div>
     </div>
   );
 }
